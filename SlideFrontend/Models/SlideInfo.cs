ec3d5321712ce81b66087ca98cbb00cf35953810
Evren Yeniev <mail@evren.dev>
2025-08-25 14:02:32 +0200

    Refactor SlideService to use SlideInfo and update _Imports.razor for new component structure

---
+++ SlideFrontend/Models/SlideInfo.cs
@@ -0,0 +1,8 @@
+namespace SlideFrontend.Models
+{
+    public class SlideInfo
+    {
+        public string Title { get; set; } = string.Empty;
+        public Type ComponentType { get; set; } = typeof(object);
+    }
+}
